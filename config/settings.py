bdd5733594bf9b64458d6469551cc995177cccfb
Aditya01JS <aditya54301@gmail.com>
2024-07-21 08:21:05 +0530

    modified settings.py

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 X_FRAME_OPTIONS = '*'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','https://freshzestjuice.netlify.app']
 
 # Application definition
 
